f718cd7669446b98eaf6973e64f5fd671a305347
Nick Breland <npbreland@tutanota.com>
2023-02-08 20:21:39 +0000

    Clean day 14 up a bit

--- 2022/14/day14.php
+++ 2022/14/day14.php
@@ -15,11 +15,18 @@ foreach ($lines as $line) {
     }, $matches[0]);
 }
 
-// Fill in grid using the line segments bormed by the point groups
-// We use a hash table for easy lookup later based on the x,y coordinates
-function buildGrid($pointGroups)
+/**
+ * Fill in grid using the line segments bormed by the point groups.
+ * We use a hash table for easy lookup later based on the x,y coordinates.
+ * Returns the grid and the max y (in physical terms, the lowest) value.
+ *
+ * @param array $pointGroups Groups of x, y coordinates that connect end to end
+ *
+ * @return array [grid, maxY]
+ */
+function buildGrid(array $pointGroups): array
 {
-    $lowestY = PHP_INT_MIN; // Keep track of the lowest y value
+    $maxY = PHP_INT_MIN; // Keep track of the lowest y value
     foreach ($pointGroups as $group) {
         for ($i = 1; $i < count($group); $i++) {
             $point = $group[$i];
@@ -36,8 +43,8 @@ function buildGrid($pointGroups)
                 $yMax = max($y1, $y2);
                 for ($y = $yMin; $y <= $yMax; $y++) {
                     $grid[$x1][$y] = true;
-                    if ($y > $lowestY) {
-                        $lowestY = $y;
+                    if ($y > $maxY) {
+                        $maxY = $y;
                     }
                 }
             } elseif ($y1 === $y2) {
@@ -45,81 +52,90 @@ function buildGrid($pointGroups)
                 $xMax = max($x1, $x2);
                 for ($x = $xMin; $x <= $xMax; $x++) {
                     $grid[$x][$y1] = true;
-                    if ($y1 > $lowestY) {
-                        $lowestY = $y1;
+                    if ($y1 > $maxY) {
+                        $maxY = $y1;
                     }
                 }
             }
         }
     }
 
-    return [
-        'grid' => $grid,
-        'lowestY' => $lowestY,
-    ];
+    return [ $grid, $maxY ];
 }
 
-$gridResult = buildGrid($pointGroups);
-$grid = $gridResult['grid'];
-$lowestY = $gridResult['lowestY'];
+list($grid, $maxY) = buildGrid($pointGroups);
+
 $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
 $grainsAtRest = 0;
 
-while ($grain[1] < $lowestY) {
+function moveGrain($grid, $grain): array
+{
+    // Possible moves
     $down = [$grain[0], $grain[1] + 1];
     $downLeft = [$grain[0] - 1, $grain[1] + 1];
     $downRight = [$grain[0] + 1, $grain[1] + 1];
+
     if (!isset($grid[$down[0]][$down[1]])) {
-        $grain = $down;
-    } elseif (!isset($grid[$downLeft[0]][$downLeft[1]])) {
-        $grain = $downLeft;
-    } elseif (!isset($grid[$downRight[0]][$downRight[1]])) {
-        $grain = $downRight;
-    } else {
-        /* Grain can't move. Stays in place, so we put it in the grid and start
-         * over with a new grain */
-        $grid[$grain[0]][$grain[1]] = true;
-        $grainsAtRest++;
-        $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
+        return [ $down, true ];
     }
+    if (!isset($grid[$downLeft[0]][$downLeft[1]])) {
+        return [ $downLeft, true ];
+    }
+    if (!isset($grid[$downRight[0]][$downRight[1]])) {
+        return [ $downRight, true ];
+    }
+
+    return [ $grain, false ];
+}
+
+function addGrainToGrid($grid, $grain)
+{
+    return [ $grid, $grain ];
+    return $grid;
+}
+
+while ($grain[1] < $maxY) {
+    list($grain, $moved) = moveGrain($grid, $grain);
+    if ($moved) {
+        continue;
+    }
+
+    /* Grain can't move. Stays in place, so we put it in the grid and start
+     * over with a new grain */
+    $grid[$grain[0]][$grain[1]] = true;
+    $grainsAtRest++;
+    $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
 }
 
 echo "Part 1. Grains at rest: $grainsAtRest" . PHP_EOL;
 
 // Part 2
-$lowestY = $lowestY + 2;
+$maxY = $maxY + 2;
 $grainsAtRest = 0;
 
-$gridResult = buildGrid($pointGroups);
-$grid = $gridResult['grid'];
+function moveGrain2($grid, $grain, $maxY)
+{
+    if ($grain[1] + 1 === $maxY) {
+        return [ $grain, false ];
+    }
+
+    return moveGrain($grid, $grain);
+}
+
+list($grid) = buildGrid($pointGroups);
 
 $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
 while (!isset($grid[SAND_SOURCE_X][SAND_SOURCE_Y])) {
-    if ($grain[1] + 1 === $lowestY) {
-        /* Grain can't move. Stays in place, so we put it in the grid and start
-         * over with a new grain */
-        $grid[$grain[0]][$grain[1]] = true;
-        $grainsAtRest++;
-        $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
+    list($grain, $moved) = moveGrain2($grid, $grain, $maxY);
+    if ($moved) {
         continue;
     }
 
-    $down = [$grain[0], $grain[1] + 1];
-    $downLeft = [$grain[0] - 1, $grain[1] + 1];
-    $downRight = [$grain[0] + 1, $grain[1] + 1];
-    if (!isset($grid[$down[0]][$down[1]]) && $down[1]) {
-        $grain = $down;
-    } elseif (!isset($grid[$downLeft[0]][$downLeft[1]])) {
-        $grain = $downLeft;
-    } elseif (!isset($grid[$downRight[0]][$downRight[1]])) {
-        $grain = $downRight;
-    } else {
-        /* Grain can't move. Stays in place, so we put it in the grid and start
-         * over with a new grain */
-        $grid[$grain[0]][$grain[1]] = true;
-        $grainsAtRest++;
-        $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
-    }
+    /* Grain can't move. Stays in place, so we put it in the grid and start
+     * over with a new grain */
+    $grid[$grain[0]][$grain[1]] = true;
+    $grainsAtRest++;
+    $grain = [SAND_SOURCE_X, SAND_SOURCE_Y];
 }
 
 echo "Part 2. Grains at rest: $grainsAtRest" . PHP_EOL;
